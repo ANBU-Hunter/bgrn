1aa92540a50c16c3b01669bcb0781f3920ee3b72
ANBU-Hunter <119634986+ANBU-Hunter@users.noreply.github.com>
2022-12-02 01:22:08 +0200

    Added All Files

---
+++ README.md
@@ -0,0 +1,11 @@
+# bgrn
+This is a Sample Tool For Automation Bug Bounty
+
+## Install:
+```
+git clone https://github.com/hunterZZzzx/bgrn.git
+```
+
+<div>
+    <img src="http://octodex.github.com/images/dojocat.jpg" style="margin: 0px; background: #0e0e0e; height: 100%">
+</div>
---
+++ bgrn.sh
@@ -0,0 +1,21 @@
+echo " █████╗ ██╗   ██╗████████╗ ██████╗ ███╗   ███╗ █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗████╗ ████║██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
+███████║██║   ██║   ██║   ██║   ██║██╔████╔██║███████║   ██║   ██║██║   ██║██╔██╗ ██║
+██╔══██║██║   ██║   ██║   ██║   ██║██║╚██╔╝██║██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝██║ ╚═╝ ██║██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
+╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+                                                                                     "
+read -p "Enter Your Domain:" Domain
+subfinder -d $Domain > subs.txt
+cat subs.txt | sort -u | tee -a new-subs.txt
+cat new-subs.txt | httprobe | tee -a alive.txt
+subzy -targets alive.txt | tee -a res-subzy.txt
+cat alive.txt | waybackurls | tee -a waybackurls.txt
+
+for arg  in {sqli,xss,ssrf,idor,urls,rce,takeovers,upload-fields,php-errors,php-sources,cors} ;
+do (cat waybackurls.txt | gf "${arg}" > $arg".txt") ;
+done
+dalfox xss.txt --custom-payload ./mypayloads.txt > dalfox-xss.txt
+nuclei -l alive.txt -t nuclei-templates -o resault.txt
+
+
