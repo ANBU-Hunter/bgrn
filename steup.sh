55c0a7437816dd335855f631be1f51a6c532fcb9
ANBU-Hunter <119634986+ANBU-Hunter@users.noreply.github.com>
2022-12-02 22:16:07 +0200

    Update steup.sh

--- steup.sh
+++ steup.sh
@@ -4,3 +4,4 @@ go install -v github.com/lukasikic/subzy@latest
 go install github.com/hahwul/dalfox/v2@latest
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+git clone https://github.com/projectdiscovery/nuclei-templates.git
