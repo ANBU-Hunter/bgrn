5f82cfb936d0a0f9ce7a72952302628fae517644
ANBU-Hunter <119634986+ANBU-Hunter@users.noreply.github.com>
2022-12-02 22:31:06 +0200

    Update bgrn.sh

--- bgrn.sh
+++ bgrn.sh
@@ -9,7 +9,7 @@ read -p "Enter Your Domain:" Domain
 subfinder -dL $Domain > subs.txt
 cat subs.txt | sort -u | tee -a new-subs.txt
 cat new-subs.txt | httprobe | tee -a alive.txt
-subzy -targets alive.txt | tee -a res-subzy.txt
+subzy -targets alive.txt -hide_fails | tee -a res-subzy.txt
 cat alive.txt | waybackurls | tee -a waybackurls.txt
 
 for arg  in {sqli,xss,ssrf,idor,urls,rce,takeovers,upload-fields,php-errors,php-sources,cors} ;
