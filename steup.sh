effd5f9b2c8f94b6ddc95e2d99e9fd5c4a6357aa
|1unter <ANBU-Hunter@protonmail.com>
2022-12-02 21:45:02 +0200

    Added  File Setup
    Co-Authored-By: ANBU-Hunter <119634986+ANBU-Hunter@users.noreply.github.com>

---
+++ steup.sh
@@ -0,0 +1,6 @@
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/tomnomnom/httprobe@latest
+go install -v github.com/lukasikic/subzy@latest
+go install github.com/hahwul/dalfox/v2@latest
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
