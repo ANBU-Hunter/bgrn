1bc6c69569cdfaa15aab0f35f92cc393331e6fc4
ANBU-Hunter <119634986+ANBU-Hunter@users.noreply.github.com>
2022-12-03 16:23:56 +0200

    Update bgrn.sh

--- bgrn.sh
+++ bgrn.sh
@@ -12,10 +12,10 @@ cat new-subs.txt | httprobe | tee -a alive.txt
 subzy -targets alive.txt -hide_fails | tee -a res-subzy.txt
 cat alive.txt | waybackurls | tee -a waybackurls.txt
 
-for arg  in {sqli,xss,ssrf,idor,urls,rce,takeovers,upload-fields,php-errors,php-sources,cors} ;
+for arg  in {sqli,xss,ssrf} ;
 do (cat waybackurls.txt | gf "${arg}" > $arg".txt") ;
 done
-dalfox xss.txt --custom-payload ./mypayloads.txt > dalfox-xss.txt
+dalfox file xss.txt --custom-payload ./mypayloads.txt > dalfox-xss.txt
 nuclei -l alive.txt -t nuclei-templates -o resault.txt
 
 
